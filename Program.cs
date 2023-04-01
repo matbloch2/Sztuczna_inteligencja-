3ff18d4888b523d1322dd66ddc56b3a73456c10b
matbloch2 <115151767+matbloch2@users.noreply.github.com>
2023-04-01 23:12:21 +0200

    Symulator wyżarzania

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+class Program
+{
+    public static double funkcja_kosztu(double x)
+    {
+        return Math.Pow(x,2);
+    }
+
+
+    static void Main()
+    {
+        
+        double T = 10000;
+        Random random = new Random();
+
+        double X=random.NextDouble()*20-10;
+        double wspułczynik_schladzania = 0.10;
+
+        while (T>1)
+        {
+            double x = random.NextDouble() * 20 -10;
+            if (funkcja_kosztu(X) - funkcja_kosztu(x) < 0)
+                X = x;
+            else
+                 if (Math.Exp(-(funkcja_kosztu(X) - funkcja_kosztu(x)) / T) < random.NextDouble())
+            {
+                X = x;
+            }
+                ///schładanie
+            T = 1 - wspułczynik_schladzania;
+        }
+
+        Console.WriteLine("x:"+X );
+        Console.WriteLine("Wartość funkcji X^2: " + funkcja_kosztu(X));
+    }
+}
